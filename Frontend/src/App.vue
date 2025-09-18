2d40050dd12dbab59b41b116dc3471be25ffc26c
purpleswtr <sergeenko.mihail@yandex.ru>
2025-09-18 16:29:59 +0300

    add frontend

---
+++ Frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <!-- Здесь будет отображаться содержимое наших будущих страниц -->
+  <RouterView />
+</template>
+
+<style scoped>
+/* Стили пока оставим пустыми */
+</style>
---
+++ Frontend/src/views/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-blue-500 text-white p-4 rounded-lg shadow-md">
+    <h1 class="text-2xl font-bold mb-2">Hello, Tailwind CSS with Vue!</h1>
+    <button class="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+      Click Me
+    </button>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
