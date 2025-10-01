b30e472a379c4c3b894eb73603c285d292491b5f
purpleswtr <sergeenko.mihail@yandex.ru>
2025-10-01 13:57:47 +0300

    f-app-button

--- Frontend/src/components/ui/AppButton.vue
+++ Frontend/src/components/ui/AppButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :disabled="props.statusLoading" class="bg-green-600 hover:bg-green-500 transition-colors rounded-xl px-3 py-2 text-white font-semibold fade">
+    <button :disabled="props.statusLoading" class="cursor-pointer bg-green-600 hover:bg-green-500 transition-colors rounded-xl px-3 py-2 text-white font-semibold fade">
     <span v-if="!props.statusLoading">{{props.message}}</span>
     <span v-else>
         Загрузка...
