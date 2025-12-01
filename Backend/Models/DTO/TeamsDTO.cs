c74b5bf9fc0461d6770644beaf30fb9f18b0f419
purpleswtr <sergeenko.mihail@yandex.ru>
2025-12-01 23:45:22 +0300

    december-backup

---
+++ Backend/Models/DTO/TeamsDTO.cs
@@ -0,0 +1,10 @@
+public class TeamCreateDto
+{
+    public string TeamName { get; set; }
+    public string? Description { get; set; }
+}
+
+public class UserToTeamDto
+{
+    public string UserId { get; set; }
+}
